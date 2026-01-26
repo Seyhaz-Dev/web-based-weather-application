653655ffca5e52df3c0fc72737d1e7c89c5c8b93
DaraCOOKED <dara.somnag020@gmail.com>
2026-01-26 13:38:42 +0700

    FOr test

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import HeaderView from '../views/HeaderView.vue'
+import ContactView from '../views/ContactView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [ 
+    {
+      path: '/',
+      name: 'header',
+      component: HeaderView,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView,
+    }
+  ],
 })
 
 export default router
---
+++ src/views/ContactView.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Dara Us</h1>
+    <p>If you have any questions, feel free to reach out!</p>
+</template>
